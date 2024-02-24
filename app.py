cdcd067a6e5c84e25cab59cd02dbfcfc0c6c3973
mohamedsiraj786 <110327550+mohamedsiraj786@users.noreply.github.com>
2024-02-24 15:28:46 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import os
+from flask import Flask, jsonify, request, render_template, redirect, url_for,session
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app = Flask(__name__, template_folder=os.path.join(BASE_DIR, "templates"))
+
+@app.route('/')
+def index():
+      return render_template('index.html')
+  
+
+if __name__ == "__main__":
+    app.run(debug=True)
