1cd8b52beb077ba52197e537911371ee5ff2d780
mohamedsiraj786 <110327550+mohamedsiraj786@users.noreply.github.com>
2024-02-24 15:58:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask import Flask, jsonify, request, render_template, redirect, url_for,se
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__, template_folder=os.path.join(BASE_DIR, "templates"))
 
-@app.route('/')
+@app.route('/https://easy-erin-moose-tam.cyclic.app/')
 def index():
       return render_template('index.html')
   
